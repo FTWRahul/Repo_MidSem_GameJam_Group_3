727c94e2c21749f1d285a8376f0d58afcc69e0ea
MarkoKuper <mkuper8@gmail.com>
2020-11-03 15:14:16 -0500

    Update MoveObjectRightToLeft.cs

--- GAMEJAM_Group3_Fall2020/Assets/_Project/Script/MoveObjectRightToLeft.cs
+++ GAMEJAM_Group3_Fall2020/Assets/_Project/Script/MoveObjectRightToLeft.cs
@@ -1,22 +1,21 @@
-﻿using Player;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveObjectRightToLeft : BasePlayerController
+public class MoveObjectRightToLeft : MonoBehaviour
 {
     public float Speed;
-    Rigidbody2D _rigidbody2D;
+    Rigidbody2D MovingObject;
 
     private void Awake()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        MovingObject = GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody2D.velocity = new Vector2(-Speed, 0);
+        MovingObject.velocity = new Vector2(-Speed, 0);
     }
 
     // Update is called once per frame
