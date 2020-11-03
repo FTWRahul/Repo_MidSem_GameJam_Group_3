a319560bd4adf15939a4724ba8e762d1a84cc518
MarkoKuper <mkuper8@gmail.com>
2020-11-03 11:32:40 -0500

    Destroy Game Object Script

---
+++ GAMEJAM_Group3_Fall2020/Assets/_Project/Script/DestroyObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public float Duration;
+
+    private void Awake()
+    {
+        Destroy(gameObject, Duration); // Destroys game object after a given duration.
+    }
+}
