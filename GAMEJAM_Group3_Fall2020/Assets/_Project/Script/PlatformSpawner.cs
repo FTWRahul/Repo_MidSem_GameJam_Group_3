b764eceaef1ca54fea2a979c63d8f76e662c1cbd
MarkoKuper <mkuper8@gmail.com>
2020-11-03 12:32:04 -0500

    Platform Spawner

---
+++ GAMEJAM_Group3_Fall2020/Assets/_Project/Script/PlatformSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    public float SpawnRate;
+    float NextSpawnTime;
+
+    public GameObject[] Platforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextSpawnTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NextSpawnTime < Time.time)
+        {
+            Instantiate(Platforms[Random.Range(0, Platforms.Length)], 
+                new Vector3(transform.position.x, transform.position.y, transform.position.z), 
+                Quaternion.identity);
+            NextSpawnTime += SpawnRate;
+        }
+    }
+}
