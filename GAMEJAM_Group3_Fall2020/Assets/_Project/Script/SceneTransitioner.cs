a6a2200de926f158b2e1942e5b88a98093300d62
MarkoKuper <mkuper8@gmail.com>
2020-11-05 10:20:14 -0500

    Scene Transitioner

---
+++ GAMEJAM_Group3_Fall2020/Assets/_Project/Script/SceneTransitioner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitioner : MonoBehaviour
+{
+    bool WasButtonPressed;
+    public Animator transition;
+    public float transitionTime = 1f;
+    public string Scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WasButtonPressed = GameObject.Find("Canvas").GetComponent<UI_Menu>().ButtonPressed;
+        if (WasButtonPressed == true)
+        {
+            StartCoroutine(LoadMyScene());
+        }
+    }
+
+    IEnumerator LoadMyScene()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(Scene);
+    }
+}
