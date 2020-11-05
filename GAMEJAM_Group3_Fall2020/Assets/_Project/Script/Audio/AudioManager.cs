79c9428d1960ee880c815defe9d4d4f3b80bb3fe
TylerFont <tylerfont@rogers.com>
2020-11-05 11:32:58 -0500

    Added AudioManager
    Added AudioManager Prefab and Audio Manager Script where we can place Audio sounds.

---
+++ GAMEJAM_Group3_Fall2020/Assets/_Project/Script/Audio/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSound[] sounds;
+
+    public static AudioManager instance;
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach (AudioSound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.audioClipName;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Start()
+    {
+        Play("GhostRealmAmbience");        
+    }
+
+    public void Play(string name)
+    {
+        AudioSound s = Array.Find(sounds, sound => sound.audioName == name);
+        s.source.Play();
+    }
+}
---
+++ GAMEJAM_Group3_Fall2020/Assets/_Project/Script/Audio/AudioSound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioSound
+{
+    public string audioName;
+
+    public AudioClip audioClipName;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0f, 5f)]
+    public float pitch;
+    
+    public bool loop;
+
+
+    [HideInInspector]
+    public AudioSource source;
+}
