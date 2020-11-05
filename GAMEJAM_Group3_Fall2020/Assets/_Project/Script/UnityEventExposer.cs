be1932c9536e5aa84e6e2fe5adc417a2d01f9422
FTWRahul <r.poundarik@gmail.com>
2020-11-05 16:53:40 -0500

    Some event stuff idk

---
+++ GAMEJAM_Group3_Fall2020/Assets/_Project/Script/UnityEventExposer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEventExposer : MonoBehaviour
+{
+    [SerializeField] private UnityEvent OnStartEvent;
+    [SerializeField] private UnityEvent OnAwakeEvent;
+    [SerializeField] private UnityEvent OnEnableEvent;
+    [SerializeField] private UnityEvent OnDisableEvent;
+
+    private void Awake()
+    {
+        OnAwakeEvent?.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        OnEnableEvent?.Invoke();
+    }
+
+    private void Start()
+    {
+        OnStartEvent?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        OnDisableEvent?.Invoke();
+    }
+}
