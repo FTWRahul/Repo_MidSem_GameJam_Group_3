c28d9abbd11148fe60c6185e0a39970bf25d649e
FTWRahul <r.poundarik@gmail.com>
2020-11-05 21:14:05 -0500

    Turn on, okay?

---
+++ GAMEJAM_Group3_Fall2020/Assets/_Project/Script/RestartAfterDelay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RestartAfterDelay : MonoBehaviour
+{
+    public void RestartSoonOKay()
+    {
+      Invoke(nameof(TurnOn), 3f);  
+    }
+
+    private void TurnOn()
+    {
+        gameObject.SetActive(true);
+    }
+}
