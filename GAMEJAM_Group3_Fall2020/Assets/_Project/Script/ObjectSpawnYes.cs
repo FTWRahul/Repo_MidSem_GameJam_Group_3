2639bad949086626d02bd0156263ad31c78bdd78
FTWRahul <r.poundarik@gmail.com>
2020-11-05 21:37:42 -0500

    more thang

---
+++ GAMEJAM_Group3_Fall2020/Assets/_Project/Script/ObjectSpawnYes.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ObjectSpawnYes : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    public void SpawnThing()
+    {
+        Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+    }
+}
